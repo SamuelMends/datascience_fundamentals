11e221a8a23aaf33bbbc4164eb0892d4b7e643d0
SamuelMends <samuelqmendes@gmail.com>
2024-08-02 20:03:26 -0300

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -1,15 +1,28 @@
 import random 
 
-
 print("Bem vindo ao jogo da forca!")
-print("Advinhe a palabra abaixo: ")
-
-
 palavras = ["banana", "uva", "manga", "melancia"]
 fruta = random.choice(palavras)
-palavra = print(fruta)
 
-spac = len(fruta)
-print('_'* spac)
-print(f'A palavra possui: {len(fruta)} letras')
+cont = 5
+
+while cont < 6:
+    print("Advinhe a palabra abaixo: ")
+    print(fruta)
+
+    spac = len(fruta)
+    print('_'* spac)
+    
+    print(f'A palavra possui: {len(fruta)} letras')
+    
+    user = str(input('Digite uma letra: '))
+    print(f"Você possui {cont} tentativas.")
+    if user not in fruta:
+        cont = cont - 1
+    if cont == 0:
+        break
+
+print('Jogo encerrado, você não possui mais tentativas.')
+    
+    
 
