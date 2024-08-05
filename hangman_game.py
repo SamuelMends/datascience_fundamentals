6279e09fe9f3406079ac13896fbf53c56db201c7
SamuelMends <samuelqmendes@gmail.com>
2024-08-05 18:49:31 -0300

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -3,34 +3,32 @@ import random
 print("Bem vindo ao jogo da forca!")
 print("\nAdvinhe a palabra abaixo: ")
 
-#Listas de palavras:
+# Listas de palavras:
 palavras = ["banana", "uva", "manga", "melancia"]
 
-#Escolha aleatória das palavras:
+# Escolha aleatória das palavras:
 fruta = random.choice(palavras)
 
-#List Comprehension:
+# List Comprehension:
 letras_descobertas = ['_' for letra in palavras]
 
-#Número de chances:
-chance = 5
+# Número de chances:
+chance = 6
 
-#Armazena letras erradas:
+# Armazena letras erradas:
 letras_erradas = []
 
-#Loop enquanto número de chances for maior do que o zero:
+# Loop enquanto número de chances for maior do que o zero:
 while chance > 0:
-
+    
+    # Print:
     print(" ".join(letras_descobertas))
     print("\nChances restantes", chance)
     print("Letras erradas:", " ".join(letras_erradas))
     
-    tentativa = str(input('Digite uma letra: ')).lower()
-    print(f"Você possui {chance} tentativas.")
-    if tentativa not in fruta:
-        chance = chance - 1
-    if chance == 0:
-        break
+    # Tentativa:
+    tentativa = input("\nDigite uma letra: ").lower()
+    
     
 
 print('Jogo encerrado, você não possui mais tentativas.')
