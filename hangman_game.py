32032f9595e83f515ad072f7a8e376008c874f4c
SamuelMends <samuelqmendes@gmail.com>
2024-08-05 18:42:24 -0300

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -6,21 +6,25 @@ fruta = random.choice(palavras)
 
 cont = 5
 
+letras_descobertas = ['_' for letra in palavras]
+
+
+
 while cont > 0:
     print("\nAdvinhe a palabra abaixo: ")
-    print(fruta)
+    letras_erradas = []
 
-    spac = len(fruta)
-    print('_' * spac)
+    print(" ".join(letras_descobertas))
+    print("\nChances restantes", cont)
+    print("Letras erradas:", " ".join(letras_erradas))
     
-    print(f'A palavra possui: {len(fruta)} Letras')
-    
-    user = str(input('Digite uma letra: '))
+    user = str(input('Digite uma letra: ')).lower()
     print(f"Você possui {cont} tentativas.")
     if user not in fruta:
         cont = cont - 1
     if cont == 0:
         break
+    
 
 print('Jogo encerrado, você não possui mais tentativas.')
     
